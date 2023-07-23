0fc9021ec3066e9c025a829afef536b800cccea2
Nicolás Delgado <delganicof29@gmail.com>
2023-07-23 16:51:15 -0500

    push

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Nicolas Delgado.
+      <time>{YEAR}</time> © Nicolás Delgado.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
