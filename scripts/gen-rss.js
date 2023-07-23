a0a6c47597ff25b81cc328d99983d7920e0c2e6a
Nicolás Delgado <delganicof29@gmail.com>
2023-07-23 16:55:21 -0500

    push

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
+    title: 'Nicolas Delgado',
     site_url: 'https://yoursite.com',
     feed_url: 'https://yoursite.com/feed.xml'
   })
